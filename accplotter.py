454a27e1f7311113326ad94183aef0ed88368799
kienjakenobi <vakeera@gmail.com>
2012-07-17 17:07:18 -0500

    working condition

--- accplotter.py
+++ accplotter.py
@@ -4,20 +4,28 @@ import sys                  # command line arguments
 import time                 # time handling
 import subprocess           # call gnulpot
 from time import strftime   # convert time to string
+import os                   # wait on gnuplot
 
 # Check for correct arguments
 if (len(sys.argv) != 4):
-    print "\nUsage: python acc-plotter.py [data.csv] [num-channels] [graph-freq]\n"
+    print "\nUsage: python acc-plotter.py [data.csv] [num-channels] \
+    [graph-freq]\n"
     print "[data.csv] is the data CSV file which should be plotted\n"
-    print "[num-channels] is the number of data channels which should be plotted"
-    print "The data channels will be plotted in the consecutive order found in the file starting with the second column, where the first column is the x axis.\n"
+    print "[num-channels] is the number of data channels which should \
+    be plotted"
+    print "The data channels will be plotted in the consecutive order found\
+     in the file starting with the second column, where the first column \
+     is the x axis.\n"
     print "[graph-freq] is the sample frequency in Hz of the plotted data."
     print "A value of 0 will use the full frequency of the given file."
-    print "Any non-zero value must be lower than the frequency of data in the given file.\n"
+    print "Any non-zero value must be lower than the frequency of data in \
+    the given file.\n"
     sys.exit()
-#
-#[start-time] is the start time of the data to be plotted.  This can be given either in the format YYYY/MM/DD@HH:MM:SS or in seconds after midnight.  A value of 0 will plot the entire time range of the given data file.  
-#
+
+#[start-time] is the start time of the data to be plotted.  
+# Give in format HH:MM
+# A value of 0 will plot the entire time range of the given data file
+
 #[end-time] is the end time range of the plot.  
 # TODO: Include the option to manually set the yrange
 
@@ -72,27 +80,35 @@ plot.stdin.write("set terminal png font \"arial,55\" size 5000,5000\n")
 plot.stdin.write("set autoscale\n")
 plot.stdin.write("set xlabel \"Time (HH:MM)\"\n")
 plot.stdin.write("set ylabel \"Accelerometer Output (Qualitative Unit)\"\n")
-plot.stdin.write("set key autotitle columnhead\n")
+plot.stdin.write("set key autotitle columnhead\n")  # Get line names from the first row of data
 plot.stdin.write("set xdata time\n")
 plot.stdin.write("set timefmt \"%s\"\n")
+plot.stdin.write("set format x \"%H:%M\"\n")
+plot.stdin.write("set title \"Vibration Data Starting at %s\"\n" % strftime("%d%b%Y at %H:%M:%S", starttime))
 # TODO: Add the duration time of the plot
-plot.stdin.write("set output '%s.png'\n" % ststr)
+plot.stdin.write("set output '%s_%dchan.png'\n" % (ststr,numchannels))
 
 # Number of lines occupied by metadata which should be skipped
-md = 9
+md = 8
 
 # Handle the plot commands
 if numchannels == 1:
     # There is only one line to plot
-    plot.stdin.write("plot '%s' every %d::%d using ($1+%d):2 with linespoints\n" % (filename, every, md, epoch))
+    plot.stdin.write("plot '%s' every %d::%d using ($1+%d-(5*3600)):2 with \
+        linespoints\n" % (filename, every, md, epoch))
     plot.stdin.write("quit\n")
+    os.waitpid(plot.pid, 0)
 else:
     # First line
-    plot.stdin.write("plot '%s' every %d::%d using ($1+%d):2 with linespoints," % (filename, every, md, epoch))
+    plot.stdin.write("plot '%s' every %d::%d using ($1+%d-(5*3600)):2 with \
+    linespoints," % (filename, every, md, epoch))
     # Loop through the middle portion of the requested channels
     for i in range(3, numchannels + 1):
-        plot.stdin.write("'%s' every %d::%d using ($1+%d):%d with linespoints," % (filename, every, md, epoch, i))
+        plot.stdin.write("'%s' every %d::%d using ($1+%d-(5*3600)):%d with \
+        linespoints," % (filename, every, md, epoch, i))
     # Final line
-    plot.stdin.write("'%s' every %d::%d using ($1+%d):%d with linespoints\n" % (filename, every, every, md, epoch, numchannels + 1))
+    plot.stdin.write("'%s' every %d::%d using ($1+%d-(5*3600)):%d with \
+    linespoints\n" % (filename, every, md, epoch, numchannels + 1))
     plot.stdin.write("quit\n")
+    os.waitpid(plot.pid, 0) # Wait for the 
     
